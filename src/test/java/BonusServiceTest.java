6c5e7c7d1e1a62d2b3cfc45d92136f38a2987b4b
Alla <thedreamtea@gmail.com>
2023-01-25 18:01:57 +1200

    ReWork

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -32,4 +32,36 @@ public class BonusServiceTest {
         // производим проверку (сравниваем ожидаемый и фактический):
         assertEquals(expected, actual);
     }
+
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForUnRegisteredAndOverLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        int amount = 55_000;
+        boolean registered = false;
+        int expected = 500;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForRegisteredAndOverLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        int amount = 20_000;
+        boolean registered = true;
+        int expected = 500;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+    }
 }
